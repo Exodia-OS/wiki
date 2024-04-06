fa9bc827d4d3ec41ca11048c0d86eadb61d12919
o0xwolf <mmsaeed509@gmail.com>
2024-04-06 13:01:56 +0200

    [1;36m==> modified : docusaurus.config.js [0m     [1;32m==> added : .github/ [0m

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Exodia OS', // Usually your GitHub org/user name.
+  organizationName: 'Exodia-OS', // Usually your GitHub org/user name.
   projectName: 'Exodia OS wiki', // Usually your repo name.
 
   onBrokenLinks: 'throw',
